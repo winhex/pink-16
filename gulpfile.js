f501b30e6bff48f7ce088b885df20cea09230bc2
winhex <minhex@gmail.com>
2019-07-08 16:28:41 +0300

    настройка среды разработки

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,28 @@
 
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
-var sourcemap = require("gulp-sourcemaps");
+
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
-    .pipe(sourcemap.init())
+    
     .pipe(less())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(sourcemap.write("."))
+    
+    .pipe(gulp.dest("source/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
@@ -25,7 +32,7 @@ gulp.task("server", function () {
   server.init({
     server: "source/",
     notify: false,
-    open: true,
+    open: false,
     cors: true,
     ui: false
   });
@@ -33,5 +40,24 @@ gulp.task("server", function () {
   gulp.watch("source/less/**/*.less", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
 });
+
+gulp.task("img-min", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+     .pipe(imagemin([
+       imagemin.optipng({optimiztionLevel: 3}),
+       imagemin.jpegtran({progressive: true}),
+       imagemin.svgo()
+     ]))
+     
+     .pipe(gulp.dest("source/img"));
+
+
+});
+
+gulp.task("webp", function () {
+   return gulp.src("source/img/**/*.{png,jpg}")
+      .pipe(webp({quality: 90}))
+      .pipe(gulp.dest("source/img"));
+ });
 
 gulp.task("start", gulp.series("css", "server"));
